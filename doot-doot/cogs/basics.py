8f002eb81c3b76f5a98aac7e9d0a9eb507bfb43e
toman222 <tobi.berger13@gmail.com>
2019-06-26 21:05:32 +0200

    Did the things and the stuff

--- doot-doot/cogs/basics.py
+++ doot-doot/cogs/basics.py
@@ -1,60 +1,59 @@
-#imports time
 import discord
 import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 import time
 from datetime import datetime
-
 import logging
 
 
 class basics(commands.Cog):
 
- def __init__(self, bot):
-     # creation of the cog, do init stuff here, also gets and stores the bot
-     self.bot: commands.Bot = bot
+    def __init__(self, bot):
+        # creation of the cog, do init stuff here, also gets and stores the bot
+        self.bot = bot
 
- def __unload(self):
-     # cog unloading, cleaup if needed
-     pass
+    def __unload(self):
+        # cog unloading, cleaup if needed
+        pass
 
- @commands.command(hidden=True)
- async def reload(self, ctx, *, cog: str):
-     cogs = []
-     cog = cog.lower()
-     for c in ctx.bot.cogs:
-         cogs.append(c.replace('Cog', '').lower())
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def reload(self, ctx, *, cog: str):
+        cogs = []
+        cog = cog.lower()
+        for c in ctx.bot.cogs:
+            cogs.append(c.replace('Cog', '').lower())
 
-     if cog in cogs:
-         self.bot.unload_extension(f"cogs.{cog}")
-         self.bot.load_extension(f"cogs.{cog}")
-         await ctx.send(f'**{cog}** has been reloaded.')
-     else:
-         await ctx.send(f"I can't find that cog.")
+        if cog in cogs:
+            self.bot.unload_extension("cogs." + cog)
+            self.bot.load_extension("cogs." + cog)
+            await ctx.send(f'**{cog}** has been reloaded.')
+        else:
+            await ctx.send(f"I can't find that cog.")
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        """kills the bot"""
+        await ctx.send("you're such a turnoff")
+        await self.bot.logout()
+        await self.bot.close()
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        """Shows the Gateway Ping"""
+        t1 = time.perf_counter()
+        await ctx.trigger_typing()
+        t2 = time.perf_counter()
+        await ctx.send(f":hourglass: gateway ping: {round((t2 - t1) * 1000)}ms :hourglass:")
+
+    @commands.command()
+    async def github(self, ctx):
+        """gives you my source code"""
+        await ctx.send("Want to report bug? \n submit feature request? \n make new feature? \n bot code is avaible on github page: \n <https://github.com/ks00908/doot-doot>")
 
 
-
- @commands.command()
- @commands.guild_only()
- @commands.has_permissions(administrator=True)
- async def shutdown(self, ctx):
-     """kills the bot"""
-     await ctx.send("you're such a turnoff")
-     await self.bot.logout()
-     await self.bot.close()
-
- @commands.command()
- async def ping(self, ctx:commands.Context):
-     """Shows the Gateway Ping"""
-     t1 = time.perf_counter()
-     await ctx.trigger_typing()
-     t2 = time.perf_counter()
-     await ctx.send(f":hourglass: gateway ping: {round((t2 - t1) * 1000)}ms :hourglass:")
-
- @commands.command()
- async def github(self, ctx):
-     """gives you my source code"""
-     await ctx.send("Want to report bug? \n submit feature request? \n make new feature? \n bot code is avaible on github page: \n <https://github.com/ks00908/doot-doot>")
 def setup(bot):
- bot.add_cog(basics(bot))
+    bot.add_cog(basics(bot))
