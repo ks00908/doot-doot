383eb4d48c721b7ae2794c5d4f6029091e95c94d
Costpap <37239383+Costpap@users.noreply.github.com>
2019-10-27 12:02:11 +0200

    Fixed spacing
    As requested in PR #24

--- doot-doot/cogs/Airhorn.py
+++ doot-doot/cogs/Airhorn.py
@@ -228,7 +228,7 @@ class Airhorn(commands.Cog):
         """Honk Honk!"""
         await play_file(ctx, "sounds/honk.mp3")
 
-   @commands.command()
+    @commands.command()
     @commands.guild_only()
     async def nofreakingway(self, ctx):
         """From the Megamind (2010) movie soundboard."""
